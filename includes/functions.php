5925f47bc53f690389c7fac42beba94fa185dcf7
patrick staats <phstaats2307@gmail.com>
2024-02-14 18:02:44 +0100

    Update loadView function PHPDoc comment

--- includes/functions.php
+++ includes/functions.php
@@ -3,8 +3,17 @@
      * Laadt de header of footer van the layout file.
      *
      * @param string $viewName De name van de view die geladen moet worden. dit moet een .php file in de layout directory zijn.
-     * @param array $headmeta Een optionele array met meta data voor de head section van de layout. Standaard is een lege array.
-    */
+     * @param array $headmeta Een optionele array met meta data voor de head section van de layout (title en author). Standaard is een lege array.
+     * 
+     * @example 
+     * $metaData = [
+     *   'pageauthor' => 'Naam van de auteur',
+     *   'pagetitle' => 'title van de pagina'
+     *];
+     *
+     * loadView('header', $metaData)
+     * loadView('footer') // geen meta data nodig
+     */
     function loadView($viewName, $metaData=[]) {
         include_once "layout/$viewName.php";
     }
