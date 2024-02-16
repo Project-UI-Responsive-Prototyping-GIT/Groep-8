b274ca9efd4a317ca8538acaa5e8bf1198400eb0
Stefanostikkie <144123423+Stefanostikkie@users.noreply.github.com>
2024-02-16 21:28:48 +0100

    Update gallery.php

--- gallery.php
+++ gallery.php
@@ -29,50 +29,6 @@
         <img src="./images/gallery foto10.png">
         </section>
         </main>
-        <footer>
-<section>
-            <img src="./images/fullLogo.png" alt="full logo">
-            <article>
-                <h3>OPENINGSTIJDEN</h3>
-                <div class="dayTime">
-                    <article>
-                        <p>Maandag: </p>
-                        <p>Dinsdag: </p>
-                        <p>Woensdag: </p>
-                        <p>Donderdag: </p>
-                        <p>Vrijdag: </p>
-                        <p>Zaterdag: </p>
-                        <p>Zondag: </p>
-                    </article>
-                    <article>
-                        <p>Gesloten</p>
-                        <p>16:00 - 22:00</p>
-                        <p>16:00 - 22:00</p>
-                        <p>16:00 - 22:00</p>
-                        <p>16:00 - 22:00</p>
-                        <p>16:00 - 22:00</p>
-                        <p>16:00 - 22:00</p>
-                    </article>
-                </div>
-            </article>
-            <div class="footer-column">
-                <article>
-                    <h3>ADRES</h3>
-                    <p>Dorpsstraat 12</p>
-                    <p>1463 BZ, Leiden</p>
-                </article>
-                <h3>071 743 17 91</h3>
-            </div>
-            <div class="footer-column">
-                <article>
-                    <h3>VOLG ONS OP</h3>
-                    <i class="fa-brands fa-facebook-f fa-2xl"></i>
-                    <i class="fa-brands fa-instagram fa-2xl"></i>
-                </article>
-                <h3>info@pizzaden.nl</h3>
-            </div>
-            <a href="#"><img src="./images/kalender.png" alt="calender om te reserveren"></a>
-        </section>
-        </footer>
+    <?php loadView('footer') ?>
     </body>
 </html>
