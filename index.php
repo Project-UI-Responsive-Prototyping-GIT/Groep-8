4f189a0d041c4cc44465f5a3e4f803c971135aa8
Stefanostikkie <144123423+Stefanostikkie@users.noreply.github.com>
2024-02-16 21:28:29 +0100

    Update index.php

--- index.php
+++ index.php
@@ -2,15 +2,33 @@
     include 'includes/functions.php';
 
     $metaData = [
-        'pageauthor' => 'P. Staats',
-        'pagetitle' => 'Ons menu'
+        'pageauthor' => 'S. Stikkelorum',
+        'pagetitle' => 'HOME'
     ];
     
     loadView('header', $metaData) 
 ?>
+<body>
 <main>
-    <!-- hier komt de inhoud -->
+        <header>
+            <?php include 'layout/functions.php'; include 'layout/header.php';  ?>
+        </header>
+    <img src="./images/Banner Home.jpg">
+    <section id="alineasOnderBanner">
+        <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Tincidunt ornare massa eget egestas purus viverra accumsan in. Mauris ultrices eros in cursus turpis massa tincidunt dui. Phasellus faucibus scelerisque eleifend donec pretium vulputate sapien. Ut morbi tincidunt augue interdum velit. Sed risus ultricies tristique nulla aliquet enim tortor. Pulvinar neque laoreet suspendisse interdum consectetur libero id faucibus nisl. Neque gravida in fermentum et sollicitudin ac. Aliquam nulla facilisi cras fermentum. Senectus et netus et malesuada fames ac turpis egestas sed. Tempor orci eu lobortis elementum. Commodo ullamcorper a lacus vestibulum. Lacinia quis vel eros donec ac odio tempor orci. Proin sed libero enim sed faucibus turpis in eu mi.</p>
+        <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Tincidunt ornare massa eget egestas purus viverra accumsan in. Mauris ultrices eros in cursus turpis massa tincidunt dui. Phasellus faucibus scelerisque eleifend donec pretium vulputate sapien. Ut morbi tincidunt augue interdum velit. Sed risus ultricies tristique nulla aliquet enim tortor. Pulvinar neque laoreet suspendisse interdum consectetur libero id faucibus nisl. Neque gravida in fermentum et sollicitudin ac. Aliquam nulla facilisi cras fermentum. Senectus et netus et malesuada fames ac turpis egestas sed. Tempor orci eu lobortis elementum. Commodo ullamcorper a lacus vestibulum. Lacinia quis vel eros donec ac odio tempor orci. Proin sed libero enim sed faucibus turpis in eu mi.</p>
+</section>
+    <section id="fotoEnAlinea3">
+        <img src="./images/image 6.png">
+        <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Tincidunt ornare massa eget egestas purus viverra accumsan in. Mauris ultrices eros in cursus turpis massa tincidunt dui. Phasellus faucibus scelerisque eleifend donec pretium vulputate sapien. Ut morbi tincidunt augue interdum velit. Sed risus ultricies tristique nulla aliquet enim tortor. Pulvinar neque laoreet suspendisse interdum consectetur libero id faucibus nisl. Neque gravida in fermentum et sollicitudin ac. Aliquam nulla facilisi cras fermentum. Senectus et netus et malesuada fames ac turpis egestas sed. Tempor orci eu lobortis elementum. Commodo ullamcorper a lacus vestibulum. Lacinia quis vel eros donec ac odio tempor orci. Proin sed libero enim sed faucibus turpis in eu mi.</p>
+
+    </section>
+    <article id="tekstOnderFoto">
+    <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Tincidunt ornare massa eget egestas purus viverra accumsan in. Mauris ultrices eros in cursus turpis massa tincidunt dui. Phasellus faucibus scelerisque eleifend donec pretium vulputate sapien. Ut morbi tincidunt augue interdum velit. Sed risus ultricies tristique nulla aliquet enim tortor. Pulvinar neque laoreet suspendisse interdum consectetur libero id faucibus nisl. Neque gravida in fermentum et sollicitudin ac. Aliquam nulla facilisi cras fermentum. Senectus et netus et malesuada fames ac turpis egestas sed. Tempor orci eu lobortis elementum. Commodo ullamcorper a lacus vestibulum. Lacinia quis vel eros donec ac odio tempor orci. Proin sed libero enim sed faucibus turpis in eu mi.</p>
+
+    </article>
 </main>
+  </body>  
 <?php
     loadView('footer')
 ?>
