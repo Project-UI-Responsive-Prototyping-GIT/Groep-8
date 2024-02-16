4035030d54f07b0a626fa5a785d970aa06cd7c94
Stefanostikkie <144123423+Stefanostikkie@users.noreply.github.com>
2024-02-16 21:19:35 +0100

    Update and rename gallery.html to gallery.php

--- gallery.php
+++ gallery.php
@@ -13,10 +13,21 @@
     </head>
     <body>
         <header>
-            <!-- hier komt de inhoud -->
+            <?php include 'layout/functions.php'; include 'layout/header.php';  ?>
         </header>
         <main>
-            <!-- hier komt de inhoud -->
+        <section id="images">
+        <img src="./images/gallery foto1.png">
+        <img src="./images/gallery foto2.png">
+        <img src="./images/gallery foto3.png">
+        <img src="./images/gallery foto4.png">
+        <img src="./images/gallery foto5.png">
+        <img src="./images/gallery foto6.png">
+        <img src="./images/gallery foto7.png">
+        <img src="./images/gallery foto8.png">
+        <img src="./images/gallery foto9.png">
+        <img src="./images/gallery foto10.png">
+        </section>
         </main>
         <footer>
             <!-- hier komt de inhoud -->
