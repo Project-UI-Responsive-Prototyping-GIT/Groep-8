48c9e81dc52835109e62a2414e073344bca03aeb
Stefano Stikkelorum <stefanostikkie@gmail.com>
2024-03-11 01:03:03 +0100

    !!!

--- aboutUs.php
+++ aboutUs.php
@@ -14,24 +14,7 @@
     <h1>About us</h1>
     <section>
         <p>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Culpa vero, 
-            omnis ad commodi, debitis vel dolorum, expedita eaque corporis magnam neque et tenetur! 
-            Deleniti perferendis ea dolores voluptates culpa sint.
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
-            Eius obcaecati dolor modi tempore doloremque perspiciatis aliquid amet voluptate reprehenderit hic architecto ea ad est, eos, 
-            expedita molestias tempora? Cum, architecto!
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Culpa vero, 
-            omnis ad commodi, debitis vel dolorum, expedita eaque corporis magnam neque et tenetur! 
-            Deleniti perferendis ea dolores voluptates culpa sint.
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
-            Eius obcaecati dolor modi tempore doloremque perspiciatis aliquid amet voluptate reprehenderit hic architecto ea ad est, eos, 
-            expedita molestias tempora? Cum, architecto!
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Culpa vero, 
-            omnis ad commodi, debitis vel dolorum, expedita eaque corporis magnam neque et tenetur! 
-            Deleniti perferendis ea dolores voluptates culpa sint.
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
-            Eius obcaecati dolor modi tempore doloremque perspiciatis aliquid amet voluptate reprehenderit hic architecto ea ad est, eos, 
-            expedita molestias tempora? Cum, architecto!
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ipsum dolor sit amet consectetur adipiscing. Neque aliquam vestibulum morbi blandit cursus risus. Sit amet mattis vulputate enim nulla aliquet porttitor lacus. Donec et odio pellentesque diam. Sed enim ut sem viverra. Enim nec dui nunc mattis enim. Molestie nunc non blandit massa enim nec dui nunc. Faucibus pulvinar elementum integer enim neque volutpat. Tortor posuere ac ut consequat semper viverra.
         </p>
         <article class="sec-one-art">
             <img src="https://picsum.photos/620/450" alt="dummy foto">
@@ -53,39 +36,11 @@
     </section>
     <section>
         <p>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Culpa vero, 
-            omnis ad commodi, debitis vel dolorum, expedita eaque corporis magnam neque et tenetur! 
-            Deleniti perferendis ea dolores voluptates culpa sint.
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
-            Eius obcaecati dolor modi tempore doloremque perspiciatis aliquid amet voluptate reprehenderit hic architecto ea ad est, eos, 
-            expedita molestias tempora? Cum, architecto!
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Culpa vero, 
-            omnis ad commodi, debitis vel dolorum, expedita eaque corporis magnam neque et tenetur! 
-            Deleniti perferendis ea dolores voluptates culpa sint.
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
-            Eius obcaecati dolor modi tempore doloremque perspiciatis aliquid amet voluptate reprehenderit hic architecto ea ad est, eos, 
-            expedita molestias tempora? Cum, architecto!
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Culpa vero, 
-            omnis ad commodi, debitis vel dolorum, expedita eaque corporis magnam neque et tenetur! 
-            Deleniti perferendis ea dolores voluptates culpa sint.
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
-            Eius obcaecati dolor modi tempore doloremque perspiciatis aliquid amet voluptate reprehenderit hic architecto ea ad est, eos, 
-            expedita molestias tempora? Cum, architecto!
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ipsum dolor sit amet consectetur adipiscing. Neque aliquam vestibulum morbi blandit cursus risus. Sit amet mattis vulputate enim nulla aliquet porttitor lacus. Donec et odio pellentesque diam. Sed enim ut sem viverra. Enim nec dui nunc mattis enim. Molestie nunc non blandit massa enim nec dui nunc. Faucibus pulvinar elementum integer enim neque volutpat. Tortor posuere ac ut consequat semper viverra.
         </p>
         <article class="sec-two-art">
             <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                Enim libero vel officia in recusandae blanditiis ipsa debitis accusamus nulla distinctio, 
-                iure eligendi est hic unde mollitia laborum sunt necessitatibus architecto?
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure voluptates beatae iste tempore 
-                architecto nisi incidunt itaque dolorum quis. Repellendus iste enim provident odio explicabo nihil 
-                sed voluptates sapiente at?
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                Enim libero vel officia in recusandae blanditiis ipsa debitis accusamus nulla distinctio, 
-                iure eligendi est hic unde mollitia laborum sunt necessitatibus architecto?
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure voluptates beatae iste tempore 
-                architecto nisi incidunt itaque dolorum quis. Repellendus iste enim provident odio explicabo nihil 
-                sed voluptates sapiente at?
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ipsum dolor sit amet consectetur adipiscing. Neque aliquam vestibulum morbi blandit cursus risus. Sit amet mattis vulputate enim nulla aliquet porttitor lacus. Donec et odio pellentesque diam. Sed enim ut sem viverra. Enim nec dui nunc mattis enim. Molestie nunc non blandit massa enim nec dui nunc. Faucibus pulvinar elementum integer enim neque volutpat. Tortor posuere ac ut consequat semper viverra.
             </p>
             <img src="https://picsum.photos/620/450" alt="dummy foto">
         </article>
